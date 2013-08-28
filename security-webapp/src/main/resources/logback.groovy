d7acb12d81c1ee442c83662f0e9a5c1427ab5e3c
blues <wangqiangsuperman123@gmail.com>
2013-08-28 23:07:44 +0800

    xxxxx

--- security-webapp/src/main/resources/logback.groovy
+++ security-webapp/src/main/resources/logback.groovy
@@ -1,6 +1,15 @@
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.core.ConsoleAppender
+
+def stream = this.class.getResourceAsStream('application.properties'),
+	props = new Properties()
+	
+props.load(stream)
+
+def level = (props['log.level'] as String).toUpperCase()
+
 appender('CONSOLE', ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        pattern = '%d{HH:mm:ss} %msg%n'
-    }
+	encoder(PatternLayoutEncoder) { pattern = '%d{HH:mm:ss} %msg%n' }
 }
-root(INFO, ['CONSOLE'])
+root(new Level(Level."${level + '_INT'}", level), ['CONSOLE'])
